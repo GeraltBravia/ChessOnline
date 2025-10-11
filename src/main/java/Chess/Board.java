a0dca35414ade102e38983fe83fa3e315ada23e2
mieuhoai <hoaihoaii0308@gmail.com>
2025-10-11 08:35:20 +0700

    chua gui

--- src/main/java/Chess/Board.java
+++ src/main/java/Chess/Board.java
@@ -1,18 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Chess;
 
 /**
- *
- * @author ACER LAPTOP
+ * Board class quản lý 8x8 ô và khởi tạo quân cờ.
  */
 public class Board {
-    Spot[][] boxes;
+    private Spot[][] boxes;
 
     public Board() {
-        this.resetBoard();
+        resetBoard();
     }
 
     public Spot getBox(int x, int y) {
@@ -24,32 +19,35 @@ public class Board {
 
     public void resetBoard() {
         boxes = new Spot[8][8];
-        // Khởi tạo quân trắng
-        boxes[0][0] = new Spot(0, 0, new Rook(true));
-        boxes[0][1] = new Spot(0, 1, new Knight(true));
-        boxes[0][2] = new Spot(0, 2, new Bishop(true));
-        boxes[0][3] = new Spot(0, 3, new Queen(true));
-        boxes[0][4] = new Spot(0, 4, new King(true));
-        boxes[0][5] = new Spot(0, 5, new Bishop(true));
-        boxes[0][6] = new Spot(0, 6, new Knight(true));
-        boxes[0][7] = new Spot(0, 7, new Rook(true));
+
+        // Khởi tạo quân đen (hàng 0–1)
+        boxes[0][0] = new Spot(0, 0, new Rook(false));
+        boxes[0][1] = new Spot(0, 1, new Knight(false));
+        boxes[0][2] = new Spot(0, 2, new Bishop(false));
+        boxes[0][3] = new Spot(0, 3, new Queen(false));
+        boxes[0][4] = new Spot(0, 4, new King(false));
+        boxes[0][5] = new Spot(0, 5, new Bishop(false));
+        boxes[0][6] = new Spot(0, 6, new Knight(false));
+        boxes[0][7] = new Spot(0, 7, new Rook(false));
         for (int j = 0; j < 8; j++) {
-            boxes[1][j] = new Spot(1, j, new Pawn(true));
+            boxes[1][j] = new Spot(1, j, new Pawn(false));
         }
-        // Khởi tạo quân đen
-        boxes[7][0] = new Spot(7, 0, new Rook(false));
-        boxes[7][1] = new Spot(7, 1, new Knight(false));
-        boxes[7][2] = new Spot(7, 2, new Bishop(false));
-        boxes[7][3] = new Spot(7, 3, new Queen(false));
-        boxes[7][4] = new Spot(7, 4, new King(false));
-        boxes[7][5] = new Spot(7, 5, new Bishop(false));
-        boxes[7][6] = new Spot(7, 6, new Knight(false));
-        boxes[7][7] = new Spot(7, 7, new Rook(false));
+
+        // Khởi tạo quân trắng (hàng 6–7)
         for (int j = 0; j < 8; j++) {
-            boxes[6][j] = new Spot(6, j, new Pawn(false));
+            boxes[6][j] = new Spot(6, j, new Pawn(true));
         }
-        // Khởi tạo các ô trống
-        for (int i = 2; i < 6; i++) {
+        boxes[7][0] = new Spot(7, 0, new Rook(true));
+        boxes[7][1] = new Spot(7, 1, new Knight(true));
+        boxes[7][2] = new Spot(7, 2, new Bishop(true));
+        boxes[7][3] = new Spot(7, 3, new Queen(true));
+        boxes[7][4] = new Spot(7, 4, new King(true));
+        boxes[7][5] = new Spot(7, 5, new Bishop(true));
+        boxes[7][6] = new Spot(7, 6, new Knight(true));
+        boxes[7][7] = new Spot(7, 7, new Rook(true));
+
+        // Khởi tạo các ô trống (hàng 2–5)
+        for (int i = 2; i <= 5; i++) {
             for (int j = 0; j < 8; j++) {
                 boxes[i][j] = new Spot(i, j, null);
             }
